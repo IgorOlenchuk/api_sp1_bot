af982b35998c187a88ad26c7811fcef857576958
Igor Olenchuk <Olenchuk.io@yandex.ru>
2020-10-03 12:14:35 +0300

    Attempt_2

--- homework.py
+++ homework.py
@@ -11,15 +11,25 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+
+    while True:
+        try:
+            homework_name = homework['homework_name']
+            status = homework['status']
+            if status == 'rejected':
+                verdict = 'К сожалению в работе нашлись ошибки.'
+            else:
+                verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+            return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+
+        except Exception as e:
+            print(f'Сервер данных не доступен: {e}')
+            time.sleep(5)
+            continue
 
 
 def get_homework_statuses(current_timestamp):
@@ -27,12 +37,19 @@ def get_homework_statuses(current_timestamp):
     data = {
         'from_date': current_timestamp
     }
-    homework_statuses = requests.get(URL, headers=headers, params=data)
-    return homework_statuses.json()
 
+    while True:
+        try:
+            homework_statuses = requests.get(URL, headers=headers, params=data)
+            print(homework_statuses.status_code)
+            return homework_statuses.json()
+
+        except Exception as e:
+            print(f'Сервер данных не доступен: {e}')
+            time.sleep(5)
+            continue
 
 def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
@@ -44,7 +61,8 @@ def main():
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
+            if new_homework.get('current_date') is not None:
+                current_timestamp = new_homework.get('current_date')  # обновить timestamp
             time.sleep(300)  # опрашивать раз в десять минут
 
         except Exception as e:
