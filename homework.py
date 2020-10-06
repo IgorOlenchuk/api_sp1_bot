d8e4e128f2be97a342f1215ec0e4cca4608c3feb
Igor Olenchuk <Olenchuk.io@yandex.ru>
2020-10-06 22:26:02 +0300

    Attempt_5

--- homework.py
+++ homework.py
@@ -33,7 +33,7 @@ def parse_homework_status(homework):
         return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
     except Exception as e:
-        logger.error(f'Error in homework_name: {e}')
+        logger.error(f'Ошибка в homework_name: {e}')
 
 
 def get_homework_statuses(current_timestamp):
